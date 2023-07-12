79299ebea9cfb1b5ea6d1120ba017e25a15acfb9
MikeGekko <MikeGekko@users.noreply.github.com>
2023-07-12 12:43:05 +0000

    deploy: e1adfdb3932cb3c3005424fa720d0fc5ae46d718

--- constants/constant.ts
+++
@@ -1,8 +0,0 @@
-export const customer = {
-  fistName: 'John',
-  lastName: 'Smith',
-  zipCode: '123456',
-};
-
-export const completeContainer =
-  'Thank you for your order!Your order has been dispatched, and will arrive just as fast as the pony can get there!';
