010bd98d00e146c60c7f8f6f08f00287585f6837
Mykhailo <mr.mike220011@gmail.com>
2023-07-12 16:31:51 +0300

    add eslint support

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:playwright/recommended'],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    'playwright/no-restricted-matchers': [
+      'warn',
+      {
+        toBeFalsy: 'Use `toBe(false)` instead.',
+        toBeTruthy: 'Use `toBe(true)` instead.',
+        not: null,
+        'not.toHaveText': null,
+      },
+    ],
+  },
+};
